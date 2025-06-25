644f5eb53974c2aa4858b3edb16401bde2c54fd5
Felix Laurent <felix.laurent@solyco.com>
2025-06-25 09:37:03 +0200

    💡 Update: main.py angepasst auf st.secrets

--- mail_utils.py
+++ mail_utils.py
@@ -4,11 +4,11 @@ from sendgrid.helpers.mail import Mail
 
 def send_email(to_email: str, subject: str, body: str) -> str:
     message = Mail(
-        from_email=os.environ["MS_USER_EMAIL"],
+        from_email=st.secrets["MS_USER_EMAIL"],
         to_emails=to_email,
         subject=subject,
         plain_text_content=body
     )
-    sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
+    sg = SendGridAPIClient(st.secrets["SENDGRID_API_KEY"])
     response = sg.send(message)
     return f"Status: {response.status_code}"
